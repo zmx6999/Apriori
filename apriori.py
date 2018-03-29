eb5da9549f1b4f19c0dbd145cc214587af5c52d3
zmx6999 <2455847459@qq.com>
2018-03-29 16:11:58 +0800

    180329

--- apriori.py
+++ apriori.py
@@ -6,7 +6,8 @@ def create_c1(data):
         _data.append(frozenset(v))
         for vv in v:
             vv = frozenset([vv])
-            if vv not in c1:c1.append(vv)
+            if vv not in c1:
+                c1.append(vv)
     return c1,_data
 
 
@@ -15,21 +16,24 @@ def get_support(data,ck,support):
     f = {}
     for v in data:
         for vv in ck:
-            if vv.issubset(v):f[vv] = f.get(vv,0)+1
+            if vv.issubset(v):
+                f[vv] = f.get(vv,0)+1
     n = len(data)
     for k in f:
         f[k] = float(f[k])/n
-        if f[k] >= support and k not in t:t.append(k)
+        if f[k] >= support:
+            t.append(k)
     return t,f
 
 
 def join(ck,length):
-    _ck = []
+    ck_1 = []
     for v in ck:
         for vv in ck:
-            m = v.union(vv)
-            if len(m) == length and m not in _ck:_ck.append(m)
-    return _ck
+            _v = v.union(vv)
+            if len(_v) == length and _v not in ck_1:
+                ck_1.append(_v)
+    return ck_1
 
 
 def subset(v):
@@ -37,35 +41,38 @@ def subset(v):
 
 
 def apriori(data,support,confidence):
-    ck,_data = create_c1(data)
-    _t,_f = get_support(_data,ck,support)
-    t,f = _t,_f
+    c1,_data = create_c1(data)
+    _t,_f = get_support(_data,c1,support)
+    t = _t
+    f = _f
     k = 2
     while len(_t) > 0:
-        ck = join(ck,k)
-        _t,_f = get_support(_data,ck,support)
+        ck = join(_t,k)
+        _t, _f = get_support(_data, ck, support)
         t.extend(_t)
         f.update(_f)
         k += 1
 
     support_data = {}
-    for v in t:support_data[tuple(v)] = f[v]
+    for v in t:
+        support_data[tuple(v)] = f[v]
 
     confidence_data = {}
     for v in t:
         if len(v) > 1:
-            ss = subset(list(v))
-            for i,sv in enumerate(ss):
+            ss = subset(v)
+            for sv in ss:
                 sv = frozenset(sv)
                 sd = v.difference(sv)
                 if len(sd) > 0:
                     cd = f[v]/f[sv]
-                    if cd >= confidence:confidence_data[tuple([tuple(sv),tuple(sd)])] = cd
+                    if cd >= confidence:
+                        confidence_data[tuple([tuple(sv),tuple(sd)])] = cd
     return support_data,confidence_data
 
-
 if __name__ == '__main__':
-    data = [v.strip().split(',') for v in open('tesco.csv', 'rU').readlines()]
-    support_data, confidence_data = apriori(data,.15,.6)
+    data = [v.strip().split(',') for v in open('tesco.csv').readlines()]
+    c1,data = create_c1(data)
+    support_data,confidence_data = apriori(data,.15,.6)
     print(support_data)
     print(confidence_data)
